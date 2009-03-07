fc311d253c3564ffcbe7700626325529867a82e9
Ryan Neufeld <neufelry@gmail.com>
2009-03-07 15:28:10 -0500

    changes

--- fitness.rb
+++ fitness.rb
@@ -5,13 +5,18 @@ class Diet
     if constraints_ok? diet
       # Start with a base score of 1000
       score = 1000
-      total_cals = diet.inject(0) {|sum,item| sum + ITEMS[item] }
+      total_cals = diet.inject(0) {|sum,item| sum + Integer($ITEMS[item][:calories]) }
       # Remove 1 pt for each calorie over or under 2000
-      score -= (2000 - total_cals).abs
+      score -= 1 * (2000 - total_cals).abs
       # Remove 5 pts of each percentage of fatty calories over 25%
       # Remove 2 pts for each 1g of protein over or under 100
+      total_protein = diet.inject(0) {|sum,item| sum + Integer($ITEMS[item][:protein]) }
+      score -= 2 * (100 - total_protein).abs
       # Remove 3 pts for each 1g of fiber over or under 20
+      total_fiber = diet.inject(0) {|sum,item| sum + Integer($ITEMS[item][:fiber]) }
+      score -= 3 * (20 - total_fiber).abs
       # Remove 50 pts for each missing portion of fruits, vegetables, milk, or meat
+      types = diet.map { |item| $ITEMS[item][:type] }.inject(Hash(0)) # NOT DONE
       # Remove 20 pts for each extra portion of fruits, vegetables, milk or meat
       # Remove 2 pts for each 1g of trans or saturated fats
     end
