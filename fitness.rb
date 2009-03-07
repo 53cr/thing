5c5760cc01918da3a783c8361819cf6b1766e4ca
Ryan Neufeld <neufelry@gmail.com>
2009-03-07 16:01:20 -0500

    Typos

--- fitness.rb
+++ fitness.rb
@@ -17,7 +17,7 @@ class Diet
       score -= 1 * (2000 - total_cals).abs
 
       # Remove 5 pts of each percentage of fatty calories over 25%
-      total_fat_over_25 = diet.percentage_of_calories_from_fat - 25
+      total_fat_over_25 = diet.percentage_of_calories_from_fats - 25
       score -= 5 * ( total_fat_over_25 > 0 ? total_fat_over_25 : 0 ) # only care about OVER
 
       # Remove 2 pts for each 1g of protein over or under 100
@@ -32,9 +32,9 @@ class Diet
       # Remove 20 pts for each extra portion of fruits, vegetables, milk or meat
       types = diet.map { |item| $ITEMS[item][:type] }
       type_totals = Hash.new(0)
-      type_totals["fruit and vegetable"] = types.find_all {|type| type == "fruit and vegetable"}
-      type_totals["milk"] = types.find_all {|type| type == "milk"}
-      type_totals["meat"] = types.find_all {|type| type == "meat"}
+      type_totals["fruit and vegetable"] = types.find_all {|type| type == "fruit and vegetable"}.size
+      type_totals["milk"] = types.find_all {|type| type == "milk"}.size
+      type_totals["meat"] = types.find_all {|type| type == "meat"}.size
 
       milk_diff = (2 - type_totals["milk"]).abs
       if type_totals["milk"] > 2
